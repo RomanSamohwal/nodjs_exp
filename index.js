2c1874ee6bbcf27861223db3c88f599ef73a2da9
RomanSamohwal <rsamohwal@gmail.com>
2020-11-09 21:53:24 +0300

    add nodemon

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const users = require('./users-router')
 const cors = require('cors')
 const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/users',{useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error',console.error.bind(console, 'connection error'))
 db.once('open', function (){
 
-})
+})*/
 
 
 const app = express()
@@ -21,10 +21,10 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.use('/users',users)
+/*app.use('/users',users)*/
 
-app.post('/tasks', async (req, res) => {
-    res.send("tasks")
+app.get('/tasks', async (req, res) => {
+    res.send("tasks tasks Roman")
 })
 
 app.use((req, res) => {
